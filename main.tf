2338b7948da7f8175d5ebafac402969aa9708e1e
thilak <tilakar1@gmail.com>
2022-11-28 23:52:21 +0000

    removing get permission

--- main.tf
+++ main.tf
@@ -98,7 +98,7 @@ data "aws_iam_policy_document" "example" {
     statement {
     effect="Allow"
     actions = [
-      "s3:GetObject",
+      #"s3:GetObject",
       "s3:PutObject"
     ]
     resources = [
