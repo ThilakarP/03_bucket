12f12dc91241d5f67f379207071f2419ef610e07
thilak <tilakar1@gmail.com>
2022-11-29 14:08:54 +0000

    adding users

---
+++ iam_user.tf
@@ -0,0 +1,30 @@
+# Definition of IAM users and groups
+
+resource "aws_iam_user" "user01" {
+  name = "user01"
+}
+
+resource "aws_iam_user" "user02" {
+  name = "user01"
+}
+
+resource "aws_iam_group" "ec2-container-registry-poweruser-group" {
+    name = "ec2-container-registry-poweruser-group"  
+}
+
+# Assign users to group
+resource "aws_iam_group_membership" "assignment" {
+  name = "assignment"
+  users = [
+    aws_iam_user.user01.name,
+    aws_iam_user.user02.name
+  ]
+  group = aws_iam_group.ec2-container-registry-poweruser-group.name   
+
+  }
+# attach policy to the group
+ resource "aws_iam_policy_attachment" "attachment" {
+   name = "attachment"
+   groups = [aws_iam_group.ec2-container-registry-poweruser-group.name]
+   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
+ }
